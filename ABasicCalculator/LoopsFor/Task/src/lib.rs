9bed09bbd342ec4cd942f1e789fb79eaf6ded4da
Sheriff332 <remotellsyou@gmail.com>
2025-12-13 17:00:24 +0530

    Day 1

--- ABasicCalculator/LoopsFor/Task/src/lib.rs
+++ ABasicCalculator/LoopsFor/Task/src/lib.rs
@@ -1,4 +1,8 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    /* TODO */
+    let mut product = 1;
+    for  i in 2..=n {
+        product *= i;
+    }
+    product
 }
--- ABasicCalculator/LoopsWhile/Task/src/lib.rs
+++ ABasicCalculator/LoopsWhile/Task/src/lib.rs
@@ -4,5 +4,11 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut product:u32 = 1;
+    let mut counter:u32 = 0;
+    while counter<n {
+        counter+=1;
+        product=product*counter;
+    }
+    product
 }
