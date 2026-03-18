ba025cc29169951b22345b6f5fe5ea608756f604
Sheriff332 <remotellsyou@gmail.com>
2026-03-18 20:58:48 +0530

    18-03-2026

--- TicketV1/Outro/Task/src/lib.rs
+++ TicketV1/Outro/Task/src/lib.rs
@@ -12,4 +12,55 @@
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
 
-/* TODO */
+pub struct Order {
+    product_name: String,
+    quantity: u32,
+    unit_price: u32
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u32, unit_price: u32) -> Self {
+        if product_name.is_empty() {
+            panic!("Product name cannot be empty");
+        } else if product_name.len()>300 {
+            panic!("Product name cannot be longer than 300 bytes");
+        } else if quantity==0 {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        } else if unit_price==0 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        Order {
+            product_name,
+            quantity,
+            unit_price
+        }
+    }
+
+    pub fn total(&self) -> u32 {
+        self.quantity * self.unit_price
+    }
+
+    // Setters
+    pub fn set_product_name(&mut self, product_name: String) {
+        self.product_name = product_name;
+    }
+    pub fn set_quantity(&mut self, quantity: u32) {
+        self.quantity = quantity;
+    }
+    pub fn set_unit_price(&mut self, unit_price: u32) {
+        self.unit_price = unit_price;
+    }
+
+    // Getters
+    pub fn product_name(&self) -> &str {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u32 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u32 {
+        &self.unit_price
+    }
+}
+
